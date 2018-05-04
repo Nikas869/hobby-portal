4d7c460d8fa97970a7ac6a42849bba90b4c343ef
Nikita Magda <nikmagdaa@gmail.com>
2018-05-04 11:12:20 +0300

    Installed Vuetify and added base layout

---
+++ HobbyPortal.WebApp/ClientApp/layout/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <app-header></app-header>
+    <v-content>
+      <v-container fluid>
+        <app-main></app-main>
+      </v-container>
+    </v-content>
+    <app-footer></app-footer>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from './Header'
+import AppFooter from './Footer'
+import AppMain from '../App'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppHeader,
+    AppFooter,
+    AppMain
+  }
+}
+</script>
+
+<style>
+
+</style>
--- HobbyPortal.WebApp/ClientApp/main.js
+++ HobbyPortal.WebApp/ClientApp/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuetify from 'vuetify'
+import AppLayout from './layout/Layout'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  template: '<AppLayout/>',
+  components: { AppLayout }
 })
