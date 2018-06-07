b639543f37b9ba8fb703861fcf498398c25e6d90
Nikita Magda <nikmagdaa@gmail.com>
2018-06-08 00:48:22 +0300

    Added profile

--- HobbyPortal.WebApp/ClientApp/pages/profile/Profile.vue
+++ HobbyPortal.WebApp/ClientApp/pages/profile/Profile.vue
@@ -1,6 +1,27 @@
 <template>
   <app-layout>
-    <v-container fluid>
+    <v-container fluid
+      text-xs-center>
+      <v-layout column justify-center>
+        <v-flex>
+          <img :src="require('../../assets/profile.png')">
+        </v-flex>
+        <v-flex>
+          <h1>
+            Ласкаво просимо, {{ this.$store.getters.user.firstName }} {{ this.$store.getters.user.lastName }}!
+          </h1>
+        </v-flex>
+        <v-flex>
+          <v-btn to="/clubs/my">
+            Мої клуби
+          </v-btn>
+        </v-flex>
+        <v-flex>
+          <v-btn @click="logout">
+            Вихід
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
   </app-layout>
 </template>
@@ -9,6 +30,11 @@
 import AppLayout from '../../layout/Layout'
 
 export default {
-  components: { AppLayout }
+  components: { AppLayout },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    }
+  }
 }
 </script>
